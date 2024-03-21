5de00618cbbdbba976b783703f9267763922b911
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-03-21 12:56:21 +0000

    Deploying to gh-pages from @ oxananazaruk/campers-for-rent@1bc2a3a7b1c2f5c986d4f86501a639c1c9475875 🚀

---
+++ assets/HomePage-20624262.js
@@ -0,0 +1,39 @@
+import{s as t,t as r,L as n,j as e}from"./index-c3a3e739.js";const o="/campers-for-rent/assets/featured-min-ca34c45c.jpg",i=t.section`
+  height: 100vh;
+  width: 100%;
+  padding-bottom: 44px;
+  background-image: linear-gradient(
+      rgba(46, 47, 66, 0.8),
+      rgba(46, 47, 66, 0.8)
+    ),
+    url(${o});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`,s=t.div`
+  width: 1280px;
+  padding: 0 20px;
+  margin: 0 auto;
+`,a=t.div`
+  padding-top: 200px;
+  display: flex;
+  gap: 40px;
+  align-items: center;
+  justify-content: center;
+`,c=t.h1`
+  font-weight: 700;
+  font-size: 54px;
+  line-height: 1.05;
+  letter-spacing: 0.01em;
+  color: ${r.color.blockzFeatures};
+  width: 550px;
+`,d=t(n)`
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 1.05;
+  letter-spacing: 0.01em;
+  color: ${r.color.blockzFeatures};
+  border: ${r.border.red};
+  border-radius: 10px;
+  padding: 17px 18px;
+`,p=()=>e.jsx(e.Fragment,{children:e.jsx(i,{children:e.jsx(s,{children:e.jsxs(a,{children:[e.jsx(c,{children:"The best way to rent an campers here"}),e.jsx(d,{to:"/first",children:"Go to campers"})]})})})}),l=()=>e.jsx(p,{});export{l as default};
